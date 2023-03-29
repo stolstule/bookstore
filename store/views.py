ba4f63dc1a7e759b5cca6ab2d033578e23fb7371
stolstule <notaz4321@Gmail.com>
2023-03-30 01:54:41 +0600

    commit 29.03.23

--- store/views.py
+++ store/views.py
@@ -6,16 +6,7 @@ from django.db.models import Max, Min
 # Create your views here.
 hud_genre_navbar = Category.objects.filter(section='Художественная литература')
 nehud_genre_navbar = Category.objects.filter(section='Нехудожественная литература')
-all_books = Book.objects.all()
-all_publishers = []
-all_authors = []
-for book in all_books:
-    if book.author not in all_authors:
-        all_authors.append(book.author)
-    if book.publisher not in all_publishers:
-        all_publishers.append(book.publisher)
 
-price_min =  Book.objects.aggregate(Min("price"))
 
 class MainPage(View):
     def get(self, request):
@@ -38,41 +29,75 @@ class ShowGenreBooks(View):
         genre = get_object_or_404(Category, slug=slug_genre)
         genre_name = genre.name
         books_by_genre = Book.objects.filter(category__name=genre_name)
+        all_publishers = []
+        all_authors = []
+        for book in books_by_genre:
+            if book.author not in all_authors:
+                all_authors.append(book.author)
+            if book.publisher not in all_publishers:
+                all_publishers.append(book.publisher)
         if genre.section == 'Художественная литература':
             genre_list = hud_genre_navbar
         else:
             genre_list = nehud_genre_navbar
-        # request = request.GET()
-        # if len(request) > 0:
-        #     request = request.GET()
-        #     books_by_genre = books_by_genre.filter(author__iexact=request['author']).filter(publisher_iexact=request['publisher'])
-        #     if len(request['price_from']) > 0 and len(request['price_to']) > 0:
-        #         books_by_genre = books_by_genre.filter(price__range=(request['price_from'], request['price_to']))
-        #     elif len(request['price_from']) < 1 and len(request['price_to']) > 0:
-        #         books_by_genre = books_by_genre.filter(price__range=(0, request['price_to']))
-        #     elif len(request['price_from']) > 0 and len(request['price_to']) < 1:
-        #         books_by_genre = books_by_genre.filter(price__range=(request['price_to'], Book.objects.aggregate(Max("price"))))
-        #
-        #     if len(request['volume_from']) > 0 and len(request['volume_to']) > 0:
-        #         books_by_genre = books_by_genre.filter(volume__range=(request['volume_from'], request['volume_to']))
-        #     elif len(request['volume_from']) < 1 and len(request['volume_to']) > 0:
-        #         books_by_genre = books_by_genre.filter(volume__range=(0, request['volume_to']))
-        #     elif len(request['volume_from']) > 0 and len(request['volume_to']) < 1:
-        #         books_by_genre = books_by_genre.filter(volume__range=(request['volume_to'], Book.objects.aggregate(Max("volume"))))
+        get_request = request.GET
+        if len(get_request) > 0:
+            if len(get_request['author']) > 0 and len(get_request['publisher']) > 0:
+                books_by_genre = books_by_genre.filter(author__iexact=get_request['author']).filter(publisher__iexact=get_request['publisher'])
+            elif len(get_request['author']) > 0 and len(get_request['publisher']) == 0:
+                books_by_genre = books_by_genre.filter(author__iexact=get_request['author'])
+            elif len(get_request['author']) == 0 and len(get_request['publisher']) > 0:
+                books_by_genre = books_by_genre.filter(publisher__iexact=get_request['publisher'])
 
-        return render(request, 'store/genre_page.html', {
-            'books_by_genre': books_by_genre,
-            'genre_name': genre_name,
-            'hud_genre_navbar': hud_genre_navbar,
-            'nehud_genre_navbar': nehud_genre_navbar,
-            'genre_list': genre_list,
-            'all_authors': sorted(all_authors),
-            'all_publishers': sorted(all_publishers),
-            'price_min': price_min,
-            'price_max': Book.objects.aggregate(Max("price")),
-            'volume_min': Book.objects.aggregate(Min("volume")),
-            'volume_max': Book.objects.aggregate(Max("price")),
-        })
+            if len(get_request['price_from']) > 0 and len(get_request['price_to']) > 0:
+                books_by_genre = books_by_genre.filter(price__range=(get_request['price_from'], get_request['price_to']))
+            elif len(get_request['price_from']) == 0 and len(get_request['price_to']) > 0:
+                books_by_genre = books_by_genre.filter(price__range=(0, get_request['price_to']))
+            elif len(get_request['price_from']) > 0 and len(get_request['price_to']) == 0:
+                books_by_genre = books_by_genre.filter(price__range=(get_request['price_from'], Book.objects.aggregate(Max('price'))['price__max']))
+
+            if len(get_request['volume_from']) > 0 and len(get_request['volume_to']) > 0:
+                books_by_genre = books_by_genre.filter(volume__range=(get_request['volume_from'], get_request['volume_to']))
+            elif len(get_request['volume_from']) == 0 and len(get_request['volume_to']) > 0:
+                books_by_genre = books_by_genre.filter(volume__range=(0, get_request['volume_to']))
+            elif len(get_request['volume_from']) > 0 and len(get_request['volume_to']) == 0:
+                books_by_genre = books_by_genre.filter(volume__range=(get_request['volume_from'], Book.objects.aggregate(Max("volume"))['volume__max']))
+
+            return render(request, 'store/genre_page.html', {
+                'count_books': len(books_by_genre),
+                'books_by_genre': books_by_genre,
+                'genre_name': genre_name,
+                'hud_genre_navbar': hud_genre_navbar,
+                'nehud_genre_navbar': nehud_genre_navbar,
+                'genre_list': genre_list,
+                'all_authors': sorted(all_authors),
+                'all_publishers': sorted(all_publishers),
+                'author_field': get_request['author'],
+                'publisher_field': get_request['publisher'],
+                'price_from_field': get_request['price_from'],
+                'price_to_field': get_request['price_to'],
+                'volume_from_field': get_request['volume_from'],
+                'volume_to_field': get_request['volume_to'],
+                'price_min': Book.objects.aggregate(Min('price'))['price__min'],
+                'price_max': Book.objects.aggregate(Max('price'))['price__max'],
+                'volume_min': Book.objects.aggregate(Min("volume"))['volume__min'],
+                'volume_max': Book.objects.aggregate(Max("volume"))['volume__max'],
+            })
+        else:
+            return render(request, 'store/genre_page.html', {
+                'count_books': len(books_by_genre),
+                'books_by_genre': books_by_genre,
+                'genre_name': genre_name,
+                'hud_genre_navbar': hud_genre_navbar,
+                'nehud_genre_navbar': nehud_genre_navbar,
+                'genre_list': genre_list,
+                'all_authors': sorted(all_authors),
+                'all_publishers': sorted(all_publishers),
+                'price_min': Book.objects.aggregate(Min('price'))['price__min'],
+                'price_max': Book.objects.aggregate(Max('price'))['price__max'],
+                'volume_min': Book.objects.aggregate(Min("volume"))['volume__min'],
+                'volume_max': Book.objects.aggregate(Max("volume"))['volume__max'],
+            })
 
 # def show_genre_books(request, slug_genre:str):
 #     genre_name = Category.objects.get(slug=slug_genre)
