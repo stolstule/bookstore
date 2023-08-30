cedfed6e28cb2400446ae751f3f572a451bce491
moon <36892483+stolstule@users.noreply.github.com>
2023-08-30 21:23:05 +0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -173,4 +173,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'notaz4321@gmail.com'
-EMAIL_HOST_PASSWORD = 'supeqtrqqyzvqnqy'
+# заполнить email_host_password
+EMAIL_HOST_PASSWORD = ''
